9aebad4fceb1e502daefb4b27f521a91a3eaf653
Albert Lockett <albert.lockett@sonraisecurity.com>
2020-03-01 16:29:12 -0400

    auth works

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
+const setupvideo = require('./video-server');
+setupvideo(app);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
---
+++ server/video-server.js
@@ -0,0 +1,99 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const { ExpressOIDC } = require('@okta/oidc-middleware');
+const okta = require('@okta/okta-sdk-nodejs');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+  region: 'us-east-1',
+});
+
+const s3 = new AWS.S3();
+function retrieveFile(path, res) {
+  const getParams = {
+    Bucket: 'lockettflix.ca',
+    Key: path.replace('/get_file/', ''),
+  };
+
+  s3.getObject(getParams, (err, data) => {
+    if (err) {
+      return res.status(400).send({ success: false, err });
+    }
+
+    return res.send(data.Body);
+  });
+}
+
+function setup(app) {
+  app.use(cookieParser());
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(express.json());
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+
+  app.use(
+    expressSession({
+      secret: process.env.APP_SECRET,
+      resave: true,
+      saveUninitialized: false,
+    }),
+  );
+
+  const oidc = new ExpressOIDC({
+    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+    client_id: process.env.OKTA_CLIENT_ID,
+    client_secret: process.env.OKTA_CLIENT_SECRET,
+    redirect_uri: `${process.env.HOST_URL}/authorization-code/callback`,
+    scope: 'openid profile',
+    appBaseUrl: 'http://localhost:3000',
+    routes: {
+      login: {
+        path: '/login',
+      },
+      callback: {
+        path: '/authorization-code/callback',
+        defaultRedirect: '/logged_in',
+      },
+    },
+  });
+
+  const oktaClient = new okta.Client({
+    orgUrl: `${process.env.OKTA_ORG_URL}`,
+    token: `${process.OKTA_TOKEN}`,
+  });
+
+  app.use(oidc.router);
+
+  app.use((req, res, next) => {
+    if (!req.userinfo) {
+      return next();
+    }
+
+    oktaClient
+      .getUser(req.userinfo.sub)
+      .then(user => {
+        req.user = user;
+        res.locals.user = user;
+        next();
+      })
+      .catch(err => {
+        next(err);
+      });
+  });
+
+  app.get('/get_file/**', oidc.ensureAuthenticated(), (req, res) => {
+    retrieveFile(req.path, res);
+  });
+}
+
+module.exports = setup;
